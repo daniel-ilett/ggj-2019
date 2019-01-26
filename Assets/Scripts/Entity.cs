eb4aa05df4c522fb56400285a0448bca54b44282
daniel-ilett <danielilett@gmail.com>
2019-01-26 22:38:37 +0000

    Set up prefabs for Entity objects.

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -5,15 +5,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using SpriteGlow;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(SpriteGlowEffect))]
 public class Entity : MonoBehaviour
 {
 	[SerializeField]
 	private Transform tooltipPivot;
 
-	private bool isPlaced = false;
+	[SerializeField]
+	private Sprite unknownSprite;
 
-	public Transform GetTooltipPivot()
+	private Sprite tooltipSprite;
+
+	private PlacementState placementState = PlacementState.Hidden;
+
+	private Color activeColour = new Color(1.0f, 1.0f, 1.0f, 0.01f);
+
+	private SpriteGlowEffect glowEffect;
+	private new SpriteRenderer renderer;
+
+	private void Awake()
 	{
+		renderer = GetComponent<SpriteRenderer>();
+		glowEffect = GetComponent<SpriteGlowEffect>();
+
+		SetState(PlacementState.Hidden);
+	}
+
+	// Set the sprite properties based on the state of the entity.
+	public void SetState(PlacementState newState)
+	{
+		placementState = newState;
+
+		switch(placementState)
+		{
+			case PlacementState.Hidden:
+				gameObject.SetActive(false);
+				break;
+
+			case PlacementState.Active:
+				gameObject.SetActive(true);
+				renderer.color = activeColour;
+				break;
+
+			case PlacementState.Placed:
+				renderer.color = Color.white;
+				glowEffect.enabled = false;
+				break;
+		}
+	}
+
+	public Transform SetTooltipActive(bool active)
+	{
+		tooltipPivot.gameObject.SetActive(active);
 		return tooltipPivot;
 	}
 }
