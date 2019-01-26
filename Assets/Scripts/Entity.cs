2bf19f295cdc2f196c183636ae5a17855e3185b9
daniel-ilett <danielilett@gmail.com>
2019-01-26 18:10:23 +0000

    Initial project commit.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,19 @@
+﻿/*	An Entity is a selectable item that is placed in the room.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+	[SerializeField]
+	private Transform tooltipPivot;
+
+	private bool isPlaced = false;
+
+	public Transform GetTooltipPivot()
+	{
+		return tooltipPivot;
+	}
+}
---
+++ Assets/Scripts/PlacementState.cs
@@ -0,0 +1,10 @@
+﻿/*	PlacementState denotes three possible states for an item -
+ *	Hidden: cannot be selected/highlighted/placed;
+ *	Active: can be hovered over and finally placed;
+ *	Placed: the item has been placed.
+ */
+
+public enum PlacementState
+{
+	Hidden, Active, Placed
+}
